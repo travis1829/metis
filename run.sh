1bcfe03c67f87cbc6a2a8c68e34b06285311a4aa
travis1829 <travis1829@naver.com>
2025-07-14 08:00:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-for i in 1 10 20 30 40 50 60 70 80
-do
-	echo ./obj/app/wrmem -s 4000 -p $i
-	perflock ./obj/app/wrmem -s 4000 -p $i
+# Warmup
+./obj/app/wrmem -s 4096 -p 64 > /dev/null
+echo
+
+for ncore in {1..2} {4..64..4}; do
+    echo "cores: $ncore"
+    ./obj/app/wrmem -s 4096 -p $ncore
+    sleep 5
 done
